063c609bf07062e5dd991eec1458b90974fa47cc
seijasm1992 <124949825+seijasm1992@users.noreply.github.com>
2025-05-22 06:35:06 -0700

    Movil version, desktop left

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,18 @@
-export default {
-  content: ['./*.html', './src/**/*.{html,js}'],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.{html,js}'],
   theme: {
     extend: {
-      fontFamily: {
-        SpaceMono: ['Space Mono', 'monospace'],
-      },
       colors: {
-        whiteOff: '#C1F2FF',
-        blueLight: '#1E2B48',
+        'blue-dark': '#141D2F',
+        'blue-light': '#1E2A47',
+        blueLight: '#1E2A47',
+      },
+      fontFamily: {
+        SpaceMono: ['"Space Mono"', 'monospace'],
+      },
+      boxShadow: {
+        custom: '0 16px 30px -10px rgba(0, 0, 0, 0.2)',
       },
     },
   },
